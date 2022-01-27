db249b5c03f7f689876bfeff992e6ffce3a4ee96
Ediz <edizyurdakul@gmail.com>
2022-01-27 15:31:20 +0200

    Edit navbar buttons

--- src/components/Layout/Navbar/index.jsx
+++ src/components/Layout/Navbar/index.jsx
@@ -1,46 +1,53 @@
 import { FaGithub } from "react-icons/fa";
 import { Link as RouteLink } from "react-router-dom";
-import { Link, Container, Flex, Box, Icon } from "@chakra-ui/react";
+import { Link, Container, Flex, Button, Icon, chakra } from "@chakra-ui/react";
 
 function Navbar() {
   return (
-    <nav>
-      <Container maxW="container.xl" py={6}>
+    <chakra.nav px={4}>
+      <Container
+        maxW="container.xl"
+        py={6}
+        bgColor="gray.800"
+        borderBottomRadius="lg"
+        boxShadow="md"
+      >
         <Flex justifyContent="space-between">
           <Flex alignItems="center">
-            <Icon boxSize={5} as={FaGithub} _hover={{ color: "purple.300" }} />
-            <Link
-              ml={4}
-              fontSize="md"
-              fontWeight="semibold"
-              as={RouteLink}
-              to="/"
-              _css={{}}
-              _hover={{ textDecoration: "none", color: "purple.300" }}
-            >
-              Github Finder
-            </Link>
+            <Button variant="ghost">
+              <Icon boxSize={5} as={FaGithub} />
+              <Link
+                ml={4}
+                fontSize="md"
+                fontWeight="semibold"
+                as={RouteLink}
+                to="/"
+                _css={{}}
+                _hover={{ textDecoration: "none" }}
+              >
+                Github Finder
+              </Link>
+            </Button>
           </Flex>
           <Flex>
-            <Link
-              as={RouteLink}
-              to="/"
-              _hover={{ textDecoration: "none", color: "purple.300" }}
-            >
-              Home
-            </Link>
-            <Link
-              ml={4}
-              as={RouteLink}
-              to="/about"
-              _hover={{ textDecoration: "none", color: "purple.300" }}
-            >
-              About
-            </Link>
+            <Button variant="ghost">
+              <Link as={RouteLink} to="/" _hover={{ textDecoration: "none" }}>
+                Home
+              </Link>
+            </Button>
+            <Button ml={2} variant="ghost">
+              <Link
+                as={RouteLink}
+                to="/about"
+                _hover={{ textDecoration: "none" }}
+              >
+                About
+              </Link>
+            </Button>
           </Flex>
         </Flex>
       </Container>
-    </nav>
+    </chakra.nav>
   );
 }
 
