563b310369bef780982d2c0123512e723638f961
Ediz <53665610+edizyurdakul@users.noreply.github.com>
2022-02-03 17:00:35 +0200

    Cleanup, refactor data fetching

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -3,31 +3,28 @@ import { useParams } from "react-router-dom";
 import GithubContext from "../context/github/GithubContext";
 import User from "../components/User";
 import SpinnerComponent from "../components/shared/Spinner";
-import { getUser, getUserRepos } from "../context/github/GithubActions";
+import { getUserAndRepos } from "../context/github/GithubActions";
 
 function UserPage() {
-  const { dispatch, user, userRepos, loading } = useContext(GithubContext);
+  const { dispatch, user, repos, loading } = useContext(GithubContext);
 
   const params = useParams();
 
   useEffect(() => {
     dispatch({ type: "SET_LOADING" });
     const getUserData = async () => {
-      const userData = await getUser(params.login);
-      dispatch({ type: "GET_USER", payload: userData });
+      const userData = await getUserAndRepos(params.login);
+      console.log(userData);
+      dispatch({ type: "GET_USER_AND_REPOS", payload: userData });
     };
-    const getUserRepoData = async () => {
-      const userRepoData = await getUserRepos(params.login);
-      dispatch({ type: "GET_REPOS", payload: userRepoData });
-    };
+
     getUserData();
-    getUserRepoData();
   }, [dispatch, params.login]);
 
   if (!loading) {
     return (
       <>
-        <User user={user} repos={userRepos} />
+        <User user={user} repos={repos} />
       </>
     );
   } else {
--- src/theme.js
+++ src/theme.js
@@ -3,7 +3,7 @@ import { mode } from "@chakra-ui/theme-tools";
 
 const config = {
   initialColorMode: "dark",
-  useSystemColorMode: false,
+  useSystemColorMode: true,
 };
 
 const styles = {
